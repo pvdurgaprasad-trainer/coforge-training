b8e9cbcdeea3270dab314d57e75cbe7d8f39c77b
Durga Prasad <venpras.tech@gmail.com>
2025-07-23 20:28:22 +0530

    initial-commit

---
+++ library-management-system/lms-be/src/main/java/venpras/tech/LmsBeApplication.java
@@ -0,0 +1,13 @@
+package venpras.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsBeApplication.class, args);
+	}
+
+}
---
+++ library-management-system/lms-be/src/test/java/venpras/tech/LmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package venpras.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
